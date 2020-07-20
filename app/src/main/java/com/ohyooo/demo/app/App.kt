2c403d9f450a268d75c9f945f299f2664daef695
ohyooo <i@ohyooo.com>
2020-07-20 22:31:57 +0800

    update lib

--- app/src/main/java/com/ohyooo/demo/app/App.kt
+++ app/src/main/java/com/ohyooo/demo/app/App.kt
@@ -1,10 +1,16 @@
 package com.ohyooo.demo.app
 
 import android.app.Application
+import timber.log.Timber
 
 class App : Application() {
     init {
         instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
     }
 
     companion object {
