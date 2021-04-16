544f0eec30f6072a8308f3044ed091a7d618f81d
csy <csy@lbx.dev>
2021-04-16 14:53:58 +0800

    update

--- buildSrc/src/main/java/com/ohyooo/version/dependencies.kt
+++ buildSrc/src/main/java/com/ohyooo/version/dependencies.kt
@@ -26,7 +26,7 @@ object Libs {
 
     object AndroidX {
         const val appcompat = "androidx.appcompat:appcompat:1.3.0-rc01"
-        const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha01"
+        const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha02"
         const val fragmentKtx = "androidx.fragment:fragment-ktx:1.3.2"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.0-beta01"
     }
